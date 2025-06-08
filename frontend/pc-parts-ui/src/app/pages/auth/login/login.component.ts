0433a61fce5fa7c1ed9efd00ca7a65308769ebe0
Nqthxn <nathanl1605@gmail.com>
2025-06-08 15:38:23 +0100

    Implemented login functionality on frontend

--- frontend/pc-parts-ui/src/app/pages/auth/login/login.component.ts
+++ frontend/pc-parts-ui/src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ){
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void{
+    if(this.loginForm.invalid){
+      return;
+    }
+
+    this.errorMessage = '';
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login successful!', response);
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.error('Login failed', err);
+        this.errorMessage = 'Login failed. Please check your username and password.';
+      }
+    });
+  }
 }
